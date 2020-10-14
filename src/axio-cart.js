35910eae14d1c644fd5dc7066cc71c880d936b7a
Jane Gao <jane.gao1108@gmail.com>
2020-10-14 00:00:33 -0700

    changed axios base url to heroku backend server

--- src/axio-cart.js
+++ src/axio-cart.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:9055'
+    baseURL: 'https://my-deer-shop.herokuapp.com/'
 });
 
 export default instance;
