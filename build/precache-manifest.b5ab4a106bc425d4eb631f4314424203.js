05f9d6bb70d70e908f717a1c3fb1b13a5f97f75d
Jane Gao <jane.gao1108@gmail.com>
2020-10-13 23:27:18 -0700

    added heroku backend server as proxy

---
+++ build/precache-manifest.b5ab4a106bc425d4eb631f4314424203.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024470464892c2bb4a1f4d5946fba3e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4c8f2f50323818ef4a70",
+    "url": "/static/css/2.dff14295.chunk.css"
+  },
+  {
+    "revision": "9566ae3d34011e004d10",
+    "url": "/static/css/main.0d33093f.chunk.css"
+  },
+  {
+    "revision": "4c8f2f50323818ef4a70",
+    "url": "/static/js/2.baa1842d.chunk.js"
+  },
+  {
+    "revision": "e3cddcc14d0b349b4e6eccba623e55ae",
+    "url": "/static/js/2.baa1842d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9566ae3d34011e004d10",
+    "url": "/static/js/main.d8870993.chunk.js"
+  },
+  {
+    "revision": "2c3f8d6c6f1635190cde",
+    "url": "/static/js/runtime-main.b6b65bc5.js"
+  },
+  {
+    "revision": "dc92d9dd9f16447a2d374f6ba5cc11d7",
+    "url": "/static/media/background.dc92d9dd.jpeg"
+  },
+  {
+    "revision": "4a76e44b790457b03d16ff1ce4fae666",
+    "url": "/static/media/logo.4a76e44b.png"
+  }
+]);
